310da065bb7700af8bc3e537852bc00810a1ce33
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-01-20 17:37:51 +0100

    webservices class

---
+++ src/main/java/rest/WebServices.java
@@ -0,0 +1,71 @@
+package rest;
+
+import domain.Home;
+import domain.Person;
+import jpa.GenTestDAO;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path( "/api" )
+public class WebServices {
+    private GenTestDAO genTestDAO = new GenTestDAO();
+
+    @GET
+    @Produces({MediaType.TEXT_PLAIN})
+    public String sayHello() {
+        return "Hello, Welcome to the Opower API";
+    }
+
+    @GET
+    @Path("/people")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response people(){
+        return this.response(this.genTestDAO.getEntityManager().
+                createQuery("from Person", Person.class).
+                getResultList());
+    }
+
+    @GET
+    @Path("/people/{id}/devices")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response personDevices(@PathParam("id") int id){
+       return this.response(this.person(id).getDevices());
+    }
+
+    @GET
+    @Path("/people/{id}/homes")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response personHomes(@PathParam("id") int id){
+        return this.response(this.person(id).getHomes());
+    }
+
+    @GET
+    @Path("/people/{id}/friends")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response personFriends(@PathParam("id") int id){
+        return this.response(this.person(id).getFriends());
+    }
+
+    @GET
+    @Path("/people/{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Person person(@PathParam("id") int id){
+        return (Person) this.genTestDAO.read(id, Person.class);
+    }
+
+    @GET
+    @Path("/homes")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response homes(){
+        return this.response(this.genTestDAO.getEntityManager().
+                createQuery("from Home", Home.class).
+                getResultList());
+    }
+
+    private Response response(Object o){
+        return Response.ok(o).build();
+    }
+
+}
