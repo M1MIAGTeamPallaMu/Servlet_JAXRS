60fa0e9e17e49d4761d05f5e578d158ab942aa36
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-02-28 11:53:38 +0100

    refactored this package after checkstyle analysis.

--- src/main/java/servlet/MyServlet.java
+++ src/main/java/servlet/MyServlet.java
@@ -1,29 +1,31 @@
 package servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 
-@WebServlet(name= "mytest" , urlPatterns={ "/myurl" })
+@WebServlet(name = "mytest", urlPatterns = {"/myurl"})
 public class MyServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req , HttpServletResponse resp )
-            throws ServletException, IOException {
-        PrintWriter p = new PrintWriter( resp .getOutputStream());
-        p.print( "Hello world SIR" );
-        p.flush();
-    }
-    @Override
-    protected void doPost(HttpServletRequest req , HttpServletResponse resp )
-            throws ServletException, IOException {
-        super .doPost( req , resp );
-    }
 
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    PrintWriter p = new PrintWriter(resp.getOutputStream());
+    p.print("Hello world SIR");
+    p.flush();
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    super.doPost(req, resp);
+  }
 
 
 }
--- src/main/java/servlet/UserInfo.java
+++ src/main/java/servlet/UserInfo.java
@@ -1,38 +1,39 @@
 package servlet;
 
 import domain.Person;
-import jpa.PersonDAO;
 
-import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name= "userInfo" , urlPatterns={ "/UserInfo" })
+import jpa.PersonDao;
+
+@WebServlet(name = "userInfo", urlPatterns = {"/UserInfo"})
 public class UserInfo extends HttpServlet {
-    @Override
-    public void doPost(HttpServletRequest request , HttpServletResponse response ) throws ServletException, IOException {
-        PersonDAO personDAO = new PersonDAO();
-        Person user = new Person();
-        user.setName(request.getParameter("name"));
-        user.setSurname(request.getParameter("firstname"));
-        user.setMail(request.getParameter("email"));
-        personDAO.create(user);
-        response .setContentType( "text/html" );
-        PrintWriter out = response .getWriter();
-        out.println( "<html>\n<body>\n" +
-                "<h1>Recapitulatif des informations</h1>\n" +
-                "<ul>\n" +
-                " <li>Nom: "
-                + request . getParameter ( "name" ) + "\n" +
-                " <li>Prenom: "
-                + request . getParameter ( "firstname" ) + "\n" +
-                " <li>Email: "
-                + request . getParameter ( "email" ) + "\n" +
-                "</ul>\n" +
-                "</body></html>" );
-    }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    Person user = new Person();
+    user.setName(request.getParameter("name"));
+    user.setSurname(request.getParameter("firstname"));
+    user.setMail(request.getParameter("email"));
+    PersonDao personDao = new PersonDao();
+    personDao.create(user);
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println("<html>\n<body>\n"
+        + "<h1>Recapitulatif des informations</h1>\n"
+        + "<ul>\n"
+        + " <li>Nom: "
+        + request.getParameter("name") + "\n"
+        + " <li>Prenom: "
+        + request.getParameter("firstname") + "\n"
+        + " <li>Email: "
+        + request.getParameter("email") + "\n"
+        + "</ul>\n" + "</body></html>");
+  }
 }
