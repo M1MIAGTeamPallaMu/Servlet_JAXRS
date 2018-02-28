364e9d40e41bf2c164d9a07cb0f09c99eb0cfb20
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-02-28 11:53:21 +0100

    refactored this package after checkstyle analysis.

--- src/main/java/rest/HomeServices.java
+++ src/main/java/rest/HomeServices.java
@@ -1,52 +1,56 @@
 package rest;
 
 import domain.Home;
-import jpa.*;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import jpa.HomeDao;
 
 @Path("/home")
 public class HomeServices {
-    private HomeDAO dao = new HomeDAO();
 
-    /**
-     * Return all homes
-     * @return
-     */
-    @GET
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response homes(){
-        return this.response(this.dao.readAll());
-    }
+  private HomeDao dao = new HomeDao();
+
+  /**
+   * Return all homes.
+   */
+  @GET
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response homes() {
+    return this.response(this.dao.readAll());
+  }
 
 
-    /**
-     * Create a new home
-     * @return
-     */
-    @POST
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response add(Home h){
-        this.dao.create(h);
-        return this.homes();
-    }
+  /**
+   * Create a new home .
+   */
+  @POST
+  @Consumes({MediaType.APPLICATION_JSON})
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response add(Home h) {
+    this.dao.create(h);
+    return this.homes();
+  }
 
-    /**
-     * Return one home by it's {id}
-     * @return
-     */
-    @GET
-    @Path("/{id}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response find(@PathParam("id") int id){
-        return this.response(this.dao.read(id));
-    }
+  /**
+   * Return one home by it's {id} .
+   */
+  @GET
+  @Path("/{id}")
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response find(@PathParam("id") int id) {
+    return this.response(this.dao.read(id));
+  }
 
-    private Response response(Object o){
-        return Response.ok(o).build();
-    }
+  private Response response(Object o) {
+    return Response.ok(o).build();
+  }
 
 }
--- src/main/java/rest/PersonServices.java
+++ src/main/java/rest/PersonServices.java
@@ -1,69 +1,79 @@
 package rest;
 
 import domain.Person;
-import jpa.*;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import jpa.PersonDao;
+
+
 @Path("/person")
 public class PersonServices {
-    private PersonDAO dao = new PersonDAO();
 
-    @GET
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response people(){
-        return this.response(this.dao.readAll());
-    }
+  private PersonDao dao = new PersonDao();
 
-    @GET
-    @Path("/{id}/devices")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response personDevices(@PathParam("id") int id){
-       return this.response(this.person(id).getDevices());
-    }
+  @GET
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response people() {
+    return this.response(this.dao.readAll());
+  }
 
-    @GET
-    @Path("/{id}/homes")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response homes(@PathParam("id") int id){
-        return this.response(this.person(id).getHomes());
-    }
+  @GET
+  @Path("/{id}/devices")
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response personDevices(@PathParam("id") int id) {
+    return this.response(this.person(id).getDevices());
+  }
 
-    @GET
-    @Path("/{id}/friends")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response friends(@PathParam("id") int id){
-        return this.response(this.person(id).getFriends());
-    }
+  @GET
+  @Path("/{id}/homes")
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response homes(@PathParam("id") int id) {
+    return this.response(this.person(id).getHomes());
+  }
 
-    @GET
-    @Path("/{id}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Person person(@PathParam("id") int id){
-        return this.dao.read(id);
-    }
+  @GET
+  @Path("/{id}/friends")
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response friends(@PathParam("id") int id) {
+    return this.response(this.person(id).getFriends());
+  }
 
-    @POST
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response add(Person p){
-        this.dao.create(p);
-        return this.people();
-    }
+  @GET
+  @Path("/{id}")
+  @Produces({MediaType.APPLICATION_JSON})
+  public Person person(@PathParam("id") int id) {
+    return this.dao.read(id);
+  }
 
-    @DELETE
-    @Path("/{id}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response delete(@PathParam("id") int id){
-        Person p = this.dao.read(id);
-        this.dao.delete(p);
-        return this.people();
-    }
+  @POST
+  @Consumes({MediaType.APPLICATION_JSON})
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response add(Person p) {
+    this.dao.create(p);
+    return this.people();
+  }
 
-    private Response response(Object o){
-        return Response.ok(o).build();
-    }
+  @DELETE
+  @Path("/{id}")
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response delete(@PathParam("id") int id) {
+    Person p = this.dao.read(id);
+    this.dao.delete(p);
+    return this.people();
+  }
+
+  private Response response(Object o) {
+    return Response.ok(o).build();
+  }
 
 }
